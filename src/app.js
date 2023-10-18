91f6bf032f70e9d77cb403758bcf3b7ee314fa63
Pratham Shailesh Dsouza <prathamshailesh777@gmail.com>
2023-10-18 22:50:38 +0530

    superwars-2

--- src/app.js
+++ src/app.js
@@ -24,32 +24,59 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
-
-    return detailedPlayers;
+    var heroes = document.querySelector("#heroes");
+    for (let i = 0; i < players.length; i++) {
+        const player = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image: `./images/super-${i + 1}.png`,
+            type: i % 2 === 0 ? 'hero' : 'villain'
+        };
+        detailedPlayers.push(player);
+    }
+        console.log(heroes)
+    return detailedPlayers
 }
+
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    var strength= Math.ceil(Math.random() * 100);
+    return strength
 }
+getRandomStrength()
 
 const buildPlayers = (players, type) => {
     let fragment = '';
-
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
+    for (let i = 0; i < players.length; i++) {
+        fragment += `<div class="player">
+            <img src="${players[i].image}">
+            <div class="details">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+            </div>
+        </div>`;
+    }
+    console.log(fragment)
+
 
     return fragment;
 }
-// Display players in HTML
 const viewPlayers = (players) => {
+    // console.log(players)
+    const heroesContainer = document.getElementById('heroes');
+    const villainsContainer = document.getElementById('villains');
 
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
-    document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
+    const heroes = players.filter(player => player.type === 'hero');
+    const villains = players.filter(player => player.type === 'villain');
+
+    heroesContainer.innerHTML = buildPlayers(heroes);
+    villainsContainer.innerHTML = buildPlayers(villains);
 
 }
 
